7b5f088940ed6ebb4adad02f810874a9eee65e05
Sheikh-ASif <mdasif74083@gmail.com>
2025-11-03 10:59:29 +0530

    Loops

--- PracticesRevision/Loops.js
+++ PracticesRevision/Loops.js
@@ -100,3 +100,72 @@ do {
     console.log(count);
     count++;
 } while ( count <= 10 );
+
+
+// for ( let i = 1; i<=10; i++){
+//     console.log(`5 *${i} = ${5 * i}`)
+// }
+
+
+// Print the reverse of a given string using a loop.
+let str = "Hello World";
+let reversedstr = "";
+for ( let i = str.length - 1; i >= 0; i-- ) {
+    reversedstr += str[i];
+}
+console.log("Reversed String:", reversedstr);
+
+// let str = "Hello World";
+// let reversedstr = str.split(' ').reverse().join(' ');
+// console.log("Reversed String:", reversedstr);
+
+
+// Count the number of digits in a given number using a loop.
+let number1 = 123456;
+let count1 = 0;
+while ( number1 != 0 ) {
+    number1 = Math.floor(number1 / 10);
+    count1++;
+}
+console.log("Number of digits:", count1);
+
+
+
+
+
+
+
+//Display the sum of even and odd numbers separately between 1 and 20.
+let sumEven = 0;
+let sumOdd = 0;
+for ( let i =1; i<=20 ; i++){
+    if( i % 2 == 0){
+        sumEven += i;
+    } else {
+        sumOdd += i;
+    }
+}
+console.log("Sum of Even numbers:", sumEven);
+console.log("Sum of Odd numbers:", sumOdd);
+
+
+// Find the largest number in an array using a loop.
+let numbers = [34, 67, 23, 89, 12];
+let largest = numbers[0];
+for ( let i = 1; i < numbers.length; i++ ) {
+    if ( numbers[i] > largest ) {
+        largest = numbers[i];
+}
+}
+console.log("Largest number is:", largest);
+
+
+//Reverse an array using a loop (without using built-in methods).
+let arr1 = [1, 2, 3, 4, 5];
+let reversedArr = [];
+for ( let i = arr1.length - 1; i >= 0; i-- ) {
+    reversedArr.push(arr1[i]);
+}
+console.log("Reversed Array:", reversedArr);
+
+
